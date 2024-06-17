8440133cd24c1c53132056b17d120bead45da46a
wangqip <wangqipengscu@outlook.com>
2024-06-17 10:39:52 +0800

    Commit on 0617

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import sqlite3
+import time
 
+import pandas as pd
 from flask import *
 
 app = Flask(__name__)
@@ -19,6 +21,88 @@ def get_db():
     return conn
 
 
+def calClusterMastery(id_list):
+    result = {
+        'mastery': [],
+        'key_date': [],
+        'key_detail': []
+    }
+    conn = get_db()
+    cur = conn.cursor()
+    sql = "SELECT ms.score, ms.time, ms.memory, ms.timeconsume,mt.knowledge FROM main.submitrecord as ms join main.titleinfo as mt WHERE student_ID in (" + ', '.join(
+        [f"'{item}'" for item in id_list]) + ") and ms.title_ID=mt.title_ID order by ms.time"
+    query_result = cur.execute(sql)
+    data = [(int(value[0]), value[1], 1, value[4]) for value in query_result]
+    df = pd.DataFrame(data, columns=['score', 'timestamp', 'count', 'knowledge'])
+    df['date'] = pd.to_datetime(df['timestamp'], unit='s')
+    daily_cumulative_sum = df.groupby(df['date'].dt.date)[['score', 'count']].sum()
+    daily_cumulative_sum['cumulative_score'] = daily_cumulative_sum['score'].cumsum()
+    daily_cumulative_sum['cumulative_count'] = daily_cumulative_sum['count'].cumsum()
+    daily_cumulative_sum['mastery'] = daily_cumulative_sum['cumulative_score'] / daily_cumulative_sum[
+        'cumulative_count']
+    result['mastery'] = [item for item in daily_cumulative_sum['mastery']]
+    quantiles = [0, 1 / 3, 2 / 3, 1]
+    knowledge = ["r8S3g", "t5V9e", "m3D1v", "s8Y2f", "k4W1c", "g7R2j", "b3C9s", "y9W5d"]
+    quantile_dates = daily_cumulative_sum.index.to_series().quantile(quantiles)
+    result['key_date'] = [str(item) for item in quantile_dates]
+    key_stamps = [time.mktime(time.strptime(str(item), '%Y-%m-%d')) for item in quantile_dates]
+    for i in range(len(quantiles) - 1):
+        for j in knowledge:
+            filtered_df = df[
+                (df['timestamp'] > key_stamps[0]) & (df['timestamp'] < key_stamps[1]) & (df['knowledge'] == j)]
+            correct_df = filtered_df[filtered_df['score'] != 0]
+            result['key_detail'].append({
+                'knowledge': j,
+                'index': i,
+                'count': filtered_df.shape[0],
+                'correctness': 0 if correct_df.shape[0] == 0 else correct_df.shape[0] / filtered_df.shape[0]
+            })
+    return result
+
+
+def insert_comma(position):
+    parts = position.strip('[]').split()
+    return [eval(parts[0]), eval(parts[1])]
+
+
+def calKnoeledgeMastery(id):
+    print(id)
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT ms.score, ms.memory, ms.timeconsume,mt.knowledge FROM main.submitrecord as ms join main.titleinfo as mt WHERE student_ID=? and ms.title_ID=mt.title_ID",
+        (id,))
+    rows = cur.fetchall()
+    if not rows:
+        return jsonify({
+            "msg": "学号无效",
+            "code": 0,
+        })
+    xAxis = {"r8S3g": 0, "t5V9e": 0, "m3D1v": 0, "s8Y2f": 0, "k4W1c": 0, "g7R2j": 0, "b3C9s": 0, "y9W5d": 0}
+    datda = {"r8S3g": [], "t5V9e": [], "m3D1v": [], "s8Y2f": [], "k4W1c": [], "g7R2j": [], "b3C9s": [], "y9W5d": []}
+    for row in rows:
+        xAxis[row[3]] += 1
+        if row[2] == '-' or row[2] == '--':
+            datda[row[3]].append([int(row[0]), int(row[1]) + 0])
+        else:
+            datda[row[3]].append([int(row[0]), int(row[1]) + int(row[2])])
+    for key in datda:
+        accumulate_score = 0
+        accumulate_index = 0
+        data = []
+        for value in datda[key]:
+            accumulate_score += value[0]
+            accumulate_index += value[1]
+            if accumulate_score == 0:
+                data.append(0)
+            else:
+                data.append(accumulate_index / accumulate_score)
+        min_value = min(data)
+        max_value = max(data)
+        datda[key] = [(value - min_value) / (max_value - min_value) for value in data]
+    return xAxis, datda
+
+
 @app.route('/')
 def hello_world():  # put application's code here
     return jsonify({
@@ -35,7 +119,7 @@ def getAnswerLog():
     try:
         cur = conn.cursor()
         cur.execute(
-            "SELECT ms.state, ms.score, mt.knowledge FROM main.submitrecord2 as ms join main.titleinfo as mt WHERE student_ID=? and ms.title_ID=mt.title_ID",
+            "SELECT ms.state, ms.score, mt.knowledge FROM main.submitrecord as ms join main.titleinfo as mt WHERE student_ID=? and ms.title_ID=mt.title_ID",
             (stu_id,))
         rows = cur.fetchall()
         if not rows:
@@ -67,7 +151,7 @@ def getKnowledgeMastery():
     try:
         cur = conn.cursor()
         cur.execute(
-            "SELECT ms.score, ms.memory, ms.timeconsume,mt.knowledge FROM main.submitrecord2 as ms join main.titleinfo as mt WHERE student_ID=? and ms.title_ID=mt.title_ID",
+            "SELECT ms.score, ms.memory, ms.timeconsume,mt.knowledge FROM main.submitrecord as ms join main.titleinfo as mt WHERE student_ID=? and ms.title_ID=mt.title_ID",
             (stu_id,))
         rows = cur.fetchall()
         if not rows:
@@ -111,6 +195,24 @@ def getKnowledgeMastery():
         conn.close()
 
 
+@app.route('/getStudyModeScatter', methods=['GET'])
+def getStudyModeScatter():
+    result = {
+        'scatter': [],
+        'detail':[]
+    }
+    data = pd.read_csv('result.csv')
+    data['position'] = data['position'].apply(insert_comma)
+    result['scatter'] = eval(data[['id', 'position', 'label']].to_json(orient='records', force_ascii=False))
+    for cluster in range(2 + 1):
+        # print(data[data['label'] == cluster]['id'].tolist())
+        result['detail'].append({
+            'detail': calClusterMastery(data[data['label'] == cluster]['id'].tolist()),
+            'index': cluster
+        })
+    return result
+
+
 # 学习模式接口
 @app.route('/getStudyMode', methods=['GET'])
 def getStudyMode():
@@ -119,7 +221,7 @@ def getStudyMode():
     try:
         cur = conn.cursor()
         cur.execute(
-            "SELECT ms.state, ms.score, mt.knowledge FROM main.submitrecord2 as ms join main.titleinfo as mt WHERE student_ID=? and ms.title_ID=mt.title_ID",
+            "SELECT ms.state, ms.score, mt.knowledge FROM main.submitrecord as ms join main.titleinfo as mt WHERE student_ID=? and ms.title_ID=mt.title_ID",
             (stu_id,))
         rows = cur.fetchall()
         if not rows:
@@ -253,3 +355,4 @@ def getStudentInfo():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # getStudyModeScatter()
